53bbbddf545532dd4ee38303c87f2e5c29cff05c
DestinedCodes <yiradesat@gmail.com>
2023-08-20 03:31:08 +0100

    Update: added f10 to run python scripts and c programs

--- init.lua
+++ init.lua
@@ -21,15 +21,12 @@ vim.opt.rtp:prepend(lazypath)
 require "plugins"
 
 -- Enable auto-save
-vim.cmd([[
-augroup autosave
-autocmd!
-autocmd TextChanged,TextChangedI <buffer> silent! write
-augroup END
-]])
-
--- Overwrite <Space>x to make the file executable
-vim.api.nvim_set_keymap('n', '<Space>l', ':!chmod +x %<CR>', { noremap = true, silent = true })
+--vim.cmd([[
+--augroup autosave
+--autocmd!
+--autocmd TextChanged,TextChangedI <buffer> silent! write
+--augroup END
+--]])
 
 -- Set tab width to 4 spaces for C files
 vim.cmd[[
@@ -42,5 +39,23 @@ vim.cmd[[
 vim.cmd([[
   autocmd FileType c autocmd InsertLeave <buffer> let b:win_view = winsaveview() | silent execute '%normal! gg=G' | call winrestview(b:win_view) | %s/\s\+$//ge
   autocmd FileType c autocmd BufWritePre <buffer> let b:win_view = winsaveview() | silent execute '%normal! gg=G' | call winrestview(b:win_view) | %s/\s\+$//ge
+]])
+
+-- Run Python programs with F10
+vim.api.nvim_exec([[
+autocmd FileType python nnoremap <buffer> <F10> :update<CR>:botright 8split term://python3 %<CR>
+]], true)
+
+-- Compile and run C programs with F10
+vim.api.nvim_exec([[
+autocmd FileType c nnoremap <buffer> <F10> :w<CR>:botright 8split term://gcc % -o %< && ./%<<CR>
+]], true)
+
+vim.cmd([[
+  augroup TerminalSettings
+    autocmd!
+    autocmd TermOpen * setlocal nonu | noremap <Esc> <Nop> | setlocal mouse=a | startinsert! | :startinsert!
+    autocmd BufEnter,CursorMoved,InsertLeave,FocusGained term://* startinsert!
+  augroup END
 ]])
 
